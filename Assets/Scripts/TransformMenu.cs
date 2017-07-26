53ee5023fe99d5338a90b8ff7726449038465348
pampas93 <abhijit.93@hotmail.com>
2017-07-26 18:19:35 -0400

    Transform Menu with icons and textures

---
+++ Assets/Scripts/TransformMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformMenu : MonoBehaviour {
+
+    public string trans = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (trans)
+        {
+            case "Move":    //Enable HandDraggable and disable rest
+                break;
+            case "Scale":   //Enable HandResize and disable rest
+                break;
+            case "Rotate":  //Enable HandRotate and disable rest
+                break;
+            default:    //Disable all three
+
+                break;
+        }
+		
+	}
+}
