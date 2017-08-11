ea0178a7d27364d1be802f270776662ec6f00577
pampas93 <abhijit.93@hotmail.com>
2017-08-11 18:00:33 -0400

    Added a few prefabs for Demo

---
+++ Assets/Temporary/InsertPrefab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using System;
+
+public class InsertPrefab : MonoBehaviour, IInputClickHandler
+{
+
+    public GameObject Prefab;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Prefab.SetActive(true);
+    }
+
+    
+}
